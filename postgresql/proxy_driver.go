7f9ecf63c3e9e680dd145ab8e2b496039ac7530d
esulzickis <esulzickis@toogoodtogo.com>
2024-12-13 10:31:14 +0100

    PLA-7464 Retry for postgresql provider

--- postgresql/proxy_driver.go
+++ postgresql/proxy_driver.go
@@ -11,7 +11,11 @@ import (
 	"golang.org/x/net/proxy"
 )
 
-const proxyDriverName = "postgresql-proxy"
+const (
+	proxyDriverName = "postgresql-proxy"
+	maxRetries      = 3
+	retryDelay      = 2 * time.Second
+)
 
 type proxyDriver struct{}
 
@@ -21,13 +25,36 @@ func (d proxyDriver) Open(name string) (driver.Conn, error) {
 
 func (d proxyDriver) Dial(network, address string) (net.Conn, error) {
 	dialer := proxy.FromEnvironment()
-	return dialer.Dial(network, address)
+	var conn net.Conn
+	var err error
+	for i := 0; i < maxRetries; i++ {
+		conn, err = dialer.Dial(network, address)
+		if err == nil {
+			return conn, nil
+		}
+		time.Sleep(retryDelay)
+	}
+	return nil, err
 }
 
 func (d proxyDriver) DialTimeout(network, address string, timeout time.Duration) (net.Conn, error) {
 	ctx, cancel := context.WithTimeout(context.TODO(), timeout)
 	defer cancel()
-	return proxy.Dial(ctx, network, address)
+	dialer := proxy.FromEnvironment()
+	var conn net.Conn
+	var err error
+	for i := 0; i < maxRetries; i++ {
+		conn, err = dialer.Dial(network, address)
+		if err == nil {
+			return conn, nil
+		}
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case <-time.After(retryDelay):
+		}
+	}
+	return nil, err
 }
 
 func init() {
